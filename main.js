4f502920bd34c4d627c96ec42cf658082d09a81a
DTurner94 <turnerdandre23@gmail.com>
2022-03-08 22:55:24 -0500

    readMeBtn

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ new Masonry("#posts.grid",{
   itemSelector :'.grid-item',
   gutter: 20
 });
+
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".text");
+
+readMoreBtn.addEventListener("click", (e) => {
+  text.classList.toggle("show-more");
+  if (readMoreBtn.innerText === "Read More") {
+
+  } else {
+    readMoreBtn.innerText = "Read More";
+  }
+});
