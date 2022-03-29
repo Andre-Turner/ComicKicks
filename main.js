c407a072113582b438c46e0882b2d3e63a5d1d99
DTurner94 <turnerdandre23@gmail.com>
2022-03-28 21:10:50 -0400

    adidasReadMoreFix

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-let toggle = document.querySelector("#header.toggle-button");
-let collapse = document.querySelectorAll("#header.collapse");
+let moreText = document.querySelector(".moreText");
 
-toggle.addEventListener('click',function(){
-  collapse.forEach(col => col.classList.toggle("collapse-toggle"));
-})
-
-// with masonry
-new Masonry("#posts.grid",{
-  itemSelector :'.grid-item',
-  gutter: 20
-});
 
 //Read More Button
 const readMoreBtn = document.querySelector(".read-more-btn");
 const text = document.querySelector(".text");
 
+// Event to show more text
 readMoreBtn.addEventListener("click", (e) => {
-  text.classList.toggle("show-more");
-  if (readMoreBtn.innerText === "Read More") {
-
-  } else {
-    readMoreBtn.innerText = "Read More";
-  }
+  moreText.style.display = "inline"
 });
